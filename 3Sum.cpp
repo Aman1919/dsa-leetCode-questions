cfb34e03ed09f925bd7576a6d94d4dd4e0a7eb88
AMAN SINGH <89124765+Aman1919@users.noreply.github.com>
2023-01-25 20:18:03 +0530

    Update 3Sum.cpp

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -1,39 +1,42 @@
 #include<iostream>
 #include<vector>
 #include<unordered_map>
+/*
+https://leetcode.com/problems/3sum/submissions/885054948/
+In this question we have return all the possible 3 number that can have sum 0 .  we can do this question by using 
+unordered_map by first inserting elements in the map then check if the sum of the two elements is present and that it's index is 
+greater than prevous elements index than insert it into ans array .
+*/
+
 
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-      if(nums.size()<3||nums[0]>0){
+        vector<vector<int>>ans;
+int sum=0;
+sort(nums.begin(),nums.end());
+if(nums.size()<3||nums[0]>0){
   return {};
 }
-  unordered_map<int,int>mp;
-        vector<vector<int>>ans;
-for( int i=0;i<nums.size();i++){
-mp[nums[i]]=i;
+unordered_map<int,int>mp;
+for(int i=0;i<nums.size();i++){
+  mp[nums[i]]=i;
 }
-
-
-
-int sum=0;
         for(int i=0;i<nums.size()-2;i++){
           if(nums[i]>0){
             break;
           }
           for(int j=i+1;j<nums.size()-1;j++){
-            
-             sum=nums[i]+nums[j];
-             if(mp.count(-sum)&&mp.find(-sum)->second>j){
+           sum=nums[i]+nums[j];
+           if(mp.count(-sum)&&mp[-sum]>j){
                ans.push_back({nums[i],nums[j],-sum});
-               j=mp.find(nums[j])->second;
-             }
-               i=mp.find(nums[i])->second;
-
+               j=mp[nums[j]];
+             
           }
+          i=mp[nums[i]];
         }
-
+        
+        }
         return ans;
     }
 };
